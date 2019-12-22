180886326c6856669f1b131da1eb487ca06dadb4
luoyong <834704671@qq.com>
2019-12-22 14:37:01 +0800

    ThreadPoolExecutor自定义线程池，自定义拒绝策略

--- thread-poll-size-magic-calculator/src/main/java/com/java/custom/CustomThreadPoolExecutor.java
+++ thread-poll-size-magic-calculator/src/main/java/com/java/custom/CustomThreadPoolExecutor.java
@@ -5,20 +5,34 @@ import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import static java.lang.Thread.sleep;
+
 public class CustomThreadPoolExecutor {
     public static void main(String[] args){
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(8,
-                16,
-                60,
-                TimeUnit.SECONDS,
-                new ArrayBlockingQueue<Runnable>(1000),
-                new CustomPolicy());
+        //　创建线程池
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1,   // 核心线程数
+                1, // 最大线程数
+                60, // 空闲非核心线程关闭时间
+                TimeUnit.SECONDS,   //时间单位
+                new ArrayBlockingQueue<Runnable>(1), // 创建工作队列
+                new CustomPolicy()); //　拒绝策略
+        for(int i = 0; i < 5; i++){
+            threadPoolExecutor.execute((Runnable)()->{
+                try {
+                    System.out.println("成功运行！！");
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        // 关闭线程池
+        threadPoolExecutor.shutdown();
     }
+    //　自定义拒绝策略
     static class CustomPolicy implements RejectedExecutionHandler {
         public void rejectedExecution(Runnable r, ThreadPoolExecutor e) {
-            System.out.println("自定义处理：开始记录日志");
-            System.out.println(r.toString());
-            System.out.println("自定义处理：记录日志完成");
+            System.out.println("-------线程任务堆积，出现任务被拒绝！！！！！");
         }
     }
 }
