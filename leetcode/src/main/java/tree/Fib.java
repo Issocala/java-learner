86180a05b467d00f97cb24e8eca42148e857e8c0
LuoYong <834704671@qq.com>
2021-03-26 10:32:23 +0800

    斐波那契数列

---
+++ leetcode/src/main/java/tree/Fib.java
@@ -0,0 +1,27 @@
+package tree;
+
+/**
+ * 斐波那契数列
+ */
+public class Fib {
+
+    public static void main(String[] args) {
+        System.out.println(fib(6));
+    }
+
+    private static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int pre = 1;
+        int curr = 1;
+        int next = 0;
+        for (int i = 3; i <=n; i++) {
+            next = pre + curr;
+            pre = curr;
+            curr = next;
+        }
+        return next;
+    }
+
+}
